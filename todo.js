5d70db24c04feeaeafe44c76a3ee2c644cbfd3f4
Andy <spiderhami@163.com>
2020-02-12 19:27:21 +0800

    完成大部分功能，剩余分类显示todos

--- todo.js
+++ todo.js
@@ -1,24 +1,76 @@
 'use strict';
 
-const componentTodo = {
-    template: '#todo',
-    props: ['isEditing', 'item']
-}
+Vue.directive('focus', {
+    inserted: function(el) {
+        el.focus();
+    }
+})
 
 const componentContainer = {
     template: '#container',
-    components: {
-        'todo-list': componentTodo
-    },
     data: function() {
         return {
-            todoList: ['aa', 'bb'],
-            isEditing: false
+            todos: [],
+            todosCompleted: [],
+            userInput: '',
+            todoCurrent: {}, // 鼠标当前停留的todo项
+            nextID: 0,
+            todoEdit: {}, // 正被编辑的todo项
+            isAllTick: false // 所有todo事项是否都已完成
+        }
+    },
+    computed: {
+        // 未完成todo数量
+        leftCount: function() {
+            const num = this.todos.length - this.todosCompleted.length;
+            if (num === 0 && this.todos.length > 0) {
+                this.isAllTick = true;
+            } else {
+                this.isAllTick = false;
+            }
+            return num;
+        },
+        tickClass: function() {
+            return {
+                black: this.isAllTick,
+                vh: this.todos.length === 0
+            }
         }
     },
     methods: {
-        toggle: function() {
-            this.isEditing = true;
+        addTodo() {
+            this.todos.push({ id: this.nextID, name: this.userInput });
+            this.nextID++;
+            this.userInput = '';
+        },
+        deleteTodo(todo, index) {
+            const i = this.todosCompleted.indexOf(todo.id); // 完成列表中的index
+            if (i !== -1) {
+                this.todosCompleted.splice(i, 1);
+            }
+            this.todos.splice(index, 1);
+        },
+        removeCompleted() {
+            this.todos = this.todos.filter(todo => {
+                return this.todosCompleted.indexOf(todo.id) === -1;
+            });
+            this.todosCompleted = [];
+        },
+        turnEdit(todo, e) {
+            if (e.target.type === 'checkbox') {
+                return;
+            }
+            this.todoEdit = todo;
+        },
+        tickTodo() {
+            if (!this.isAllTick) {
+                this.todosCompleted = this.todos.reduce((arr, todo) => {
+                    arr.push(todo.id);
+                    return arr;
+                }, []);
+            } else {
+                this.todosCompleted = [];
+            }
         }
     }
 };
